c58e4de86b6ffd0eaf96373f743b48c5b9a088e5
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-02-05 09:52:56 +0100

    Add blog app. Add basic fields and content to home page.

--- wagtailblog/home/models.py
+++ wagtailblog/home/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.fields import RichTextField
 
 
 class HomePage(Page):
-    pass
+    
+    call_to_action = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    description = RichTextField(
+        null=True,
+        blank=True
+    )
+
+    # Editor panels configuration
+    content_panels = Page.content_panels + [
+        # FieldPanel('header_image'),
+        # FieldPanel('date'),
+        FieldPanel("call_to_action"),
+        FieldPanel('description'),
+    ]
